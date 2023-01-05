2afc13ac1cd4a25babb41c4cab60ed58245ffc6c
kissjgabi <b6@sryer.com>
2023-01-05 20:28:32 +0100

    Update exec_php_from_github.php

--- exec_php_from_github.php
+++ exec_php_from_github.php
@@ -1,18 +1,17 @@
 <?php
 
-function eval_php_from_git($GithubRawURI){
+function eval_php_from_git($GithubRawURI)
+{
+    echo $GithubRawURI;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close($ch);
+    //remove the "<?php" from the file before running it
+    $data = substr($data, 5);
 
-//remove the "<?php" from the file before running it
-$data = substr($data, 5);
-
-eval ($data);
+    eval($data);
 }
-?>
-<!-- EoF -->
