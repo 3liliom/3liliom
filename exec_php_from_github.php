16d72e30f57307c809611890397900b006b06912
kissjgabi <b6@sryer.com>
2023-01-05 20:30:21 +0100

    Update exec_php_from_github.php

--- exec_php_from_github.php
+++ exec_php_from_github.php
@@ -2,7 +2,6 @@
 
 function eval_php_from_git($GithubRawURI)
 {
-    echo $GithubRawURI;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -14,4 +13,6 @@ function eval_php_from_git($GithubRawURI)
     $data = substr($data, 5);
 
     eval($data);
+
+    echo $GithubRawURI . '<br />';
 }
