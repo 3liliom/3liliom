e3cd26bba7dd9ffe4193cdf0ed4d9c21bc3e74cc
kissjgabi <b6@sryer.com>
2023-01-05 20:09:15 +0100

    Update 3liliom_styles.php

--- root-php/3liliom_styles.php
+++ root-php/3liliom_styles.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../root-css/3liliom_style.css">
+<link rel="stylesheet" href="https://raw.githubusercontent.com/3liliom/3liliom.github.io/main/root-css/3liliom_style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <style>
