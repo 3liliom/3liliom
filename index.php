315b301291cae10338292262da8b6e92a129f233
kissjgabi <b6@sryer.com>
2023-01-05 18:00:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
+include_once 'exec_php_from_github.php';
 $root_php = 'https://raw.githubusercontent.com/3liliom/3liliom.github.io/main/root-php/';
 ?>
 
 <!DOCTYPE html>
 <html lang="hu">
 
-<?php include $root_php . '3liliom_front.php'; ?>
+<?php eval_php_from_git($root_php . '3liliom_front.php'); ?>
 
 </html>
