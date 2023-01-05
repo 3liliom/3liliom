86038ed111435c90d930275c32a67ce4f95f60c0
kissjgabi <b6@sryer.com>
2023-01-05 12:54:16 +0100

    Create 3liliom_head.php

---
+++ root-php/3liliom_head.php
@@ -0,0 +1,37 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <?php
+    $liliom_db_title = "3liliom";
+    ?>
+
+    <title>
+        <?php echo $liliom_db_title; ?>
+    </title>
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+    <meta name="keywords" content="egészség, Neolife, gyógyhatású tea, mandala" />
+    <meta name="description" content="Neolife webáruház és gyógyhatású teák terjesztése" />
+    <meta name="subject" content="Neolife webáruház és gyógyhatású teák terjesztése" />
+    <meta name="author" content="Gabor J. Kiss">
+
+    <meta name="revised" content='<?php echo date("m/d/Y") ?>' />
+    <meta name="revisit-after" content="3 days" />
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="initial-scale=1.0, 
+				 maximum-scale=1.0, 
+				 minimum-scale=1.0, 
+				 user-scalable=no, 
+				 minimal-ui" />
+    <meta name="apple-mobile-web-app-title" content="3 liliom db" />
+
+    <?php
+    include_once "3liliom_styles.php";
+    ?>
+
+</head>
