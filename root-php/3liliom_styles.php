5945cb026db34c109c3f63d69be43c4fdee74e1d
kissjgabi <b6@sryer.com>
2023-01-05 20:51:01 +0100

    Delete 3liliom_styles.php

--- root-php/3liliom_styles.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-<link rel="stylesheet" href="https://raw.githubusercontent.com/3liliom/3liliom.github.io/main/root-css/3liliom_style.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
-<style>
-
-</style>
