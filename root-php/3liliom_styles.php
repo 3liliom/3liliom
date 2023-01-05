246aec57ed5c1cf73937b45e084d5489417308c5
kissjgabi <b6@sryer.com>
2023-01-05 12:57:43 +0100

    Create 3liliom_styles.php

---
+++ root-php/3liliom_styles.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../root-css/3liliom_style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+
+</style>
