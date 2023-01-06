20feebf4f741c2bb88caca35a48e3cea186a047d
3liliom <122012389+3liliom@users.noreply.github.com>
2023-01-06 20:59:31 +0100

    u

--- root-js/3liliom_functions.js
+++ root-js/3liliom_functions.js
@@ -9,3 +9,17 @@ function toggleFunction() {
         x.className = x.className.replace(" b6-show", "");
     }
 }
+
+// Change style of navbar on scroll
+window.onscroll = function () {
+    changeTopNavbarStyle()
+};
+
+function changeTopNavbarStyle() {
+    var navbar = document.getElementById("topNavbar");
+    if (document.body.scrollTop > 96 || document.documentElement.scrollTop > 96) {
+        navbar.className = "b6-bar" + " b6-card" + " b6-animate-top" + " b6-white";
+    } else {
+        navbar.className = navbar.className.replace(" b6-card b6-animate-top b6-white", "");
+    }
+}
