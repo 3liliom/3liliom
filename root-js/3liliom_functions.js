05c4a387a276f74c810904d202033ea13dae51be
3liliom <122012389+3liliom@users.noreply.github.com>
2023-01-06 17:52:26 +0100

    update

--- root-js/3liliom_functions.js
+++ root-js/3liliom_functions.js
@@ -1,1 +1,11 @@
 var the_3liliom_date = "2023.01.06";
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("b6-show") == -1) {
+        x.className += " b6-show";
+    } else {
+        x.className = x.className.replace(" b6-show", "");
+    }
+}
