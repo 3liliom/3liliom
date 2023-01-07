edfce4840beb088f0a429c0217f53e74ae56b77f
kissjgabi <b6@sryer.com>
2023-01-07 19:16:51 +0100

    Update 3liliom_root_01_inc_functions.js

--- 3liliom_root_js/3liliom_root_01_inc_functions.js
+++ 3liliom_root_js/3liliom_root_01_inc_functions.js
@@ -1,6 +1,7 @@
-
-getText("3liliom_root_html/3liliom_root_index_header.html", "home");
-getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
+function loadContents() {
+    getText("3liliom_root_html/3liliom_root_index_header.html", "home");
+    getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
+}
 
 async function getText(file, id) {
     let myObject = await fetch(file);
