dd85e7ccad959660aae1d3216fad1c9715e0c582
kissjgabi <b6@sryer.com>
2023-01-07 17:58:06 +0100

    Create 3liliom_root_01_inc_functions.js

---
+++ 3liliom_root_js/3liliom_root_01_inc_functions.js
@@ -0,0 +1,11 @@
+// document.write("Copyright &copy; <script> document.write(new Date().getFullYear())</script> All rights reserved.");
+
+getText("3liliom_root_html/3liliom_root_index_head.html", "index_head");
+getText("3liliom_root_html/3liliom_root_index_header.html", "home");
+getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
+
+async function getText(file, id) {
+    let myObject = await fetch(file);
+    let myText = await myObject.text();
+    document.getElementById(id).innerHTML = myText;
+}
