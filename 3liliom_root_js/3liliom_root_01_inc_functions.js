2f259b4cec49a89a604042ae780194aa83816a15
kissjgabi <b6@sryer.com>
2023-01-07 18:02:00 +0100

    Update 3liliom_root_01_inc_functions.js

--- 3liliom_root_js/3liliom_root_01_inc_functions.js
+++ 3liliom_root_js/3liliom_root_01_inc_functions.js
@@ -1,6 +1,4 @@
-// document.write("Copyright &copy; <script> document.write(new Date().getFullYear())</script> All rights reserved.");
 
-getText("3liliom_root_html/3liliom_root_index_head.html", "index_head");
 getText("3liliom_root_html/3liliom_root_index_header.html", "home");
 getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
 
