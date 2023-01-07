ddc56095d81cbaa7be1eb8d31d6db4b03353a0d1
kissjgabi <b6@sryer.com>
2023-01-07 16:10:16 +0100

    Update 3liliom_root_00_inc_functions.js

--- 3liliom_root_js/3liliom_root_00_inc_functions.js
+++ 3liliom_root_js/3liliom_root_00_inc_functions.js
@@ -1,15 +1,13 @@
 document.write("Copyright &copy; <script> document.write(new Date().getFullYear())</script> All rights reserved.");
 
-let file = "3liliom_root_html/3liliom_root_index_header.html";
-fetch(file)
-.then(x => document.getElementById("home").innerHTML += x.text() + "<hr />")
-.then(y => document.getElementById("home").innerHTML += y);
-
 document.getElementById("home").innerHTML += "Step 1: <hr />";
-
-file = "3liliom_root_html/3liliom_root_index_footer.html";
-fetch(file)
-.then(x => document.getElementById("home").innerHTML += x.text() + "<hr />")
-.then(y => document.getElementById("bottom").innerHTML += y);
+getText("3liliom_root_html/3liliom_root_index_header.html", "home");
 
 document.getElementById("bottom").innerHTML = "Step 2: <hr />";
+getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
+
+async function getText(file, id) {
+    let myObject = await fetch(file);
+    let myText = await myObject.text();
+    document.getElementById(id).innerHTML = myText;
+}
