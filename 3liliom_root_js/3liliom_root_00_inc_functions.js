32d3bd7c7010bc57f3f2f3c82d8fa8c3af4c79f2
kissjgabi <b6@sryer.com>
2023-01-07 16:21:57 +0100

    Update 3liliom_root_00_inc_functions.js

--- 3liliom_root_js/3liliom_root_00_inc_functions.js
+++ 3liliom_root_js/3liliom_root_00_inc_functions.js
@@ -1,9 +1,6 @@
 document.write("Copyright &copy; <script> document.write(new Date().getFullYear())</script> All rights reserved.");
 
-document.getElementById("home").innerHTML += "Step 1: <hr />";
 getText("3liliom_root_html/3liliom_root_index_header.html", "home");
-
-document.getElementById("bottom").innerHTML = "Step 2: <hr />";
 getText("3liliom_root_html/3liliom_root_index_footer.html", "bottom");
 
 async function getText(file, id) {
