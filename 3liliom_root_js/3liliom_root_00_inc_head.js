254cdc1daf6c08c7bf63097f8f8a906479ba01e8
kissjgabi <b6@sryer.com>
2023-01-07 18:01:28 +0100

    Update 3liliom_root_00_inc_head.js

---
+++ 3liliom_root_js/3liliom_root_00_inc_head.js
@@ -0,0 +1,9 @@
+// read&write to 3liliom.hu/index.html head
+
+getText("3liliom_root_html/3liliom_root_index_head.html");
+
+async function getText(file) {
+    let myObject = await fetch(file);
+    let myText = await myObject.text();
+    document.write(myText);
+}
