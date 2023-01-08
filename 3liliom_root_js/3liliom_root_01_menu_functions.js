0e3df65c4fd9571de5f1ead99a1e4e6ccc58eabf
kissjgabi <b6@sryer.com>
2023-01-08 20:41:17 +0100

    Update 3liliom_root_01_menu_functions.js

--- 3liliom_root_js/3liliom_root_01_menu_functions.js
+++ 3liliom_root_js/3liliom_root_01_menu_functions.js
@@ -3,10 +3,10 @@ var the_3liliom_date = "2023.01.06";
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
     var x = document.getElementById("toggleNavbar");
-    if (x.className.indexOf("b6-show") == -1) {
+    if (x.className.indexOf("show") == -1) {
         x.className += " b6-show";
     } else {
-        x.className = x.className.replace(" b6-show", "");
+        x.className = x.className.replace(" show", "");
     }
 }
 
